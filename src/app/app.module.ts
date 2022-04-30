17a52cbfe3d425150b44a499508b9757f533de4f
Nora Youssefi <80409227+NoraYoussefi@users.noreply.github.com>
2022-04-30 12:26:00 +0000

    commit 2

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductComponent } from './product/product.component';
+import { BasketComponent } from './basket/basket.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { FiltersComponent } from './filters/filters.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent,
+    ProductComponent,
+    BasketComponent,
+    ContactComponent,
+    AboutComponent,
+    CategoriesComponent,
+    FiltersComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
